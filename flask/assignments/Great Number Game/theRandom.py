6f48873d8f490fdd45702a91fe35d5cee3981591
mohannad saadeh <155642672+MohnadSaadeh@users.noreply.github.com>
2024-03-02 15:35:08 +0200

    new functionality to the game

--- flask/assignments/Great Number Game/theRandom.py
+++ flask/assignments/Great Number Game/theRandom.py
@@ -6,18 +6,32 @@ app.secret_key = 'my random'
 
 @app.route('/')
 def theroot1():
-    session['rand'] =  random.randint(1,100)
+    session['rand'] =  int(random.randint(1,100))
     session['theGuessed'] = 0
+    session['attempts'] = 0
+    session['attemptsUser'] = 10
+
     print(session['rand'])
     return render_template("index.html")
+
+
+# Transfrom Data from fromtEnd To BackEnt 
 
 @app.route('/yourguess' , methods = ['POST'])
 def theguss():
     session['theGuessed'] = int(request.form['yournu_num']) 
+    session['attempts'] += 1 
+    session['attemptsUser'] -= 1
+    print(session['attempts'])
     return redirect('/toredirect')
 @app.route('/toredirect')
 def theRediFun():
     return render_template("index.html")
+
+#Transfrom Data from fromtEnd To BackEnt
+
+
+
 
 @app.route('/clear')
 def theclear():
@@ -26,4 +40,6 @@ def theclear():
 
 
 if __name__=="__main__":
-    app.run(debug=True)
+    # app.run(debug=True)
+    #app.run(host='192.168.1.12')
+    app.run(host='192.168.1.12' , port=1200, debug=True)
