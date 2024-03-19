972756e346a4431f1b8ade2a725dc8cc2f740ac1
mohannad saadeh <155642672+MohnadSaadeh@users.noreply.github.com>
2024-03-19 04:29:43 +0200

    modification to the game

--- django/django_fundamentals/GNG_Django/Greate_Number_Game/views.py
+++ django/django_fundamentals/GNG_Django/Greate_Number_Game/views.py
@@ -6,23 +6,33 @@ def theroot1(request):
     request.session['rand'] =  int(random.randint(1,100))
     request.session['theGuessed'] = 0
     request.session['attempts'] = 0
-    request.session['attemptsUser'] = 10
-    # print(request.session['rand'])
-    
-    context = {
-        "rand" : request.session['rand'],
-        "theGuessed" : request.session['theGuessed'],
-        "attempts" : request.session['attempts'] ,
-        "attemptsUser" : request.session['attemptsUser']
-    }
-    return render(request , "index.html" , context)
+    request.session['attemptsUser'] = 5
+
+    return render(request , "index.html" )
 
 def yourguess(request):
     request.session['yournu_num'] =   request.POST['yournu_num']
-    
-    if (int(request.session['yournu_num']) > request.session['rand']):
-        return render(request , "high.html")
+    request.session['attempts'] += 1
+    request.session['attemptsUser'] -= 1
+    grater = "helper_color"
+    less = "helper_color2"
+    equal = "helper_color3"
+
+    messegBIG = "Too High"
+    messegLESS = "Too Low"
+    messegEQUAL = "Is True"
+    if (request.session['attemptsUser'] == 0):
+        return render(request , "loose.html")
+    elif (int(request.session['yournu_num']) > request.session['rand']):
+        request.session['color'] = grater
+        request.session['resultMesseg'] = messegBIG
+        return render(request , "index.html")
     elif (int(request.session['yournu_num']) < request.session['rand']):
-        return render(request , "low.html")
+        request.session['color'] = less
+        request.session['resultMesseg'] = messegLESS
+        return render(request , "index.html")
     elif (int(request.session['yournu_num']) == request.session['rand']):
-        return render(request , "equal.html")
+        request.session['color'] = equal
+        request.session['resultMesseg'] = messegEQUAL
+        return render(request , "index.html")
+    
