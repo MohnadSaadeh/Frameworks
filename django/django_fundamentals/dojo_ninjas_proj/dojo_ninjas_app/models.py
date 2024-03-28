fe69e5515dadc8626771730cb58bba621207456f
mohannad saadeh <155642672+MohnadSaadeh@users.noreply.github.com>
2024-03-28 14:57:35 +0200

    Dojo ninja

---
+++ django/django_fundamentals/dojo_ninjas_proj/dojo_ninjas_app/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+# Create your models here.
+class Dojo(models.Model):
+    name = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+    state = models.CharField(max_length=255)
+    desc = models.TextField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    # ninjas = list of ninjas associated with a givin dogo
+    def __repr__(self):
+        return "Dojo_name: {}".format(self.name)
+
+class Ninja(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    dojo = models.ForeignKey(Dojo, related_name="ninjas", on_delete = models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def __repr__(self):
+        return "first_name: {}".format(self.first_name)
